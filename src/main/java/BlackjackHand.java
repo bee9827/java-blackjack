203d848f6cd6097bb5771a9a0793b23a2d572f78
bee9827 <ehfrhfo9494@naver.com>
2026-03-04 19:59:41 +0900

    feat: 사용자의 핸드를 관리하는 BlackjackHand 생성
    Rank -> 카드의 숫자를 관리하는 책임이 있다.
    Card -> 카드의 값이 무엇인지 반환할 책임이 있다.
    BlackjackHand -> 받은 카드들을 이용한 함수, 일급 컬렉션

---
+++ src/main/java/BlackjackHand.java
@@ -0,0 +1,55 @@
+import java.util.List;
+
+public class BlackjackHand {
+    public static final int ACE_PROFIT_VALUE = 10;
+    private static final int BLACKJACK_MAX_SCORE = 21;
+    private static final int MIN_SIZE = 2;
+    private static final int MIN_TOTAL_SCORE = 2;
+
+    private List<Card> hands;
+
+    public BlackjackHand(List<Card> cards) {
+        validateCardsSize(cards);
+        hands = cards;
+    }
+
+    // 버스트 확인
+    public static boolean isBurst(int totalScore) {
+        if (totalScore < MIN_TOTAL_SCORE) {
+            throw new IllegalArgumentException(ErrorMessage.BURST_TOTAL_SCORE.getMessage());
+        }
+        return totalScore > BLACKJACK_MAX_SCORE;
+    }
+
+    private void validateCardsSize(List<Card> cards) { //테스트 작성
+        if (cards.size() != MIN_SIZE) {
+            throw new IllegalArgumentException(ErrorMessage.HANDS_CARDS_SIZE.getMessage());
+        }
+    }
+
+    public int getTotalScore() {    // 테스트
+        int totalScore = hands.stream()
+                .map(Card::getScore)
+                .reduce(Integer::sum)
+                .orElse(0);
+
+        if (totalScore <= BLACKJACK_MAX_SCORE - ACE_PROFIT_VALUE && hasAce(hands)) {
+            return totalScore + ACE_PROFIT_VALUE;
+        }
+
+        return totalScore;
+    }
+
+    public int getHandsSize() {
+        return hands.size();
+    }
+
+    public void add(Card card) {
+        hands.add(card);
+    }
+
+    private boolean hasAce(List<Card> hands) {
+        return hands.stream()
+                .anyMatch(Card::isAce);
+    }
+}
